ca181957ca3d0a276406ec68736f4f741220f9f2
pioupia <49518790+pioupia@users.noreply.github.com>
2023-02-23 20:47:45 +0100

    Fix: Crashing when passing parameters (#11)

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ import { draw, parse } from "./util/common";
 
 draw(
     parse(`
+    const compose = (...fns) => res => fns.reduce((accum, next) => next(accum), res);
 const test = true;
 const pluckDeep = key => test;
 
 if (test) console.log(false);
-    
+
 const bj = 1;
 const tj = "test";
 `, Prism.languages.javascript),
