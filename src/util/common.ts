4d2e8c330e6d4743c1048070c18ec922eba08ebc
pioupia <pioupiadeux@gmail.com>
2023-02-23 15:58:22 +0100

    Feat: creating the getHeightOfAText function and tests

--- src/util/common.ts
+++ src/util/common.ts
@@ -1,22 +1,56 @@
-import { createCanvas, loadImage } from 'canvas';
+import { createCanvas, loadImage, CanvasRenderingContext2D } from 'canvas';
 import Prism, { Grammar } from 'prismjs';
+import { ImageSizes } from '../types/common';
 
+function getCharHeight(metrics: TextMetrics) {
+    return metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+}
 
-/**
- * Margin:
- *  - 15px left + 30px right
- *  - 20px bottom
- *  - 20px top + layout of 50px
- * @param data
- * @param width
- */
-function evaluateHeight(data: (string | Prism.Token)[], width: number) {
-    let lastX = 15; // margin: 15px
-    let lastY = 70; // margin: 20px + layout;
+function getHeightOfAText(ctx: CanvasRenderingContext2D, charHeight: number,
+                          text: string, lastX: number, lastY: number, width: number,
+                          textLength?: number): number[] {
+    textLength ||= text.length;
+
+    let lastIndexSpace = 0;
+    for (let i = 0; i < textLength; i++) {
+        const charWidth = ctx.measureText(text[i]).width;
+        if(text[i] === ' ') lastIndexSpace = i;
+
+        if ((lastX + charWidth + ImageSizes.marginRight) > width) {
+            const sentenceWidth = ctx.measureText(text.slice(0, lastIndexSpace)).width;
+            let cuttingIndex = i - 1;
+            if (sentenceWidth + lastY + ImageSizes.marginRight <= width) {
+                cuttingIndex = lastIndexSpace + 1;
+            }
+
+            text = text.slice(cuttingIndex);
+            textLength = textLength - cuttingIndex;
+
+            lastY += ImageSizes.textLineHeight + charHeight;
+            lastX = ImageSizes.marginLeft;
+        }
+
+        lastX += charWidth;
+    }
+
+    return [lastX, lastY];
+}
+
+export function evaluateHeight(data: (string | Prism.Token)[], width: number) {
+    let lastX = ImageSizes.marginLeft;
+    let lastY = ImageSizes.marginTop * 2 + ImageSizes.headerHeight;
+    const ctx = createCanvas(200, 200).getContext('2d');
+    const charHeight = getCharHeight(ctx.measureText(']'));
 
     for (const part of data) {
-
+        if (typeof part === "string") {
+            [lastX, lastY] = getHeightOfAText(ctx, charHeight, part, lastX, lastY, width);
+        } else {
+            [lastX, lastY] = getHeightOfAText(ctx, charHeight, part.content.toString(), lastX, lastY, width, part.length);
+        }
     }
+
+    return lastY + ImageSizes.marginBottom + charHeight;
 }
 function draw() {
 
