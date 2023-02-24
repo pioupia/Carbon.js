c8af04f6b4cf39a9345b5661fff4e9ac5024adb0
pioupia <pioupiadeux@gmail.com>
2023-02-24 09:13:14 +0100

    Refactor: prettify the code

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import { parse } from "./util/common";
 import { draw } from "./util/generateImage";
 import CarbonjsError from "./errors/CarbonjsErrors";
 
-
 /**
  * Allows you to generate an Image from code
  * @param {string} code
@@ -12,12 +11,13 @@ import CarbonjsError from "./errors/CarbonjsErrors";
  * @param {number=} customWidth The custom with of the image (default: 750px)
  * @return {Canvas}
  */
-export default function carbon(code: string, language: Grammar, customWidth?: number): Canvas {
-    if (typeof customWidth === "number" && customWidth <= 100)
-        throw new CarbonjsError("The 'customWidth' can't be less than 100.");
+export default function carbon(
+  code: string,
+  language: Grammar,
+  customWidth?: number
+): Canvas {
+  if (typeof customWidth === "number" && customWidth <= 100)
+    throw new CarbonjsError("The 'customWidth' can't be less than 100.");
 
-    return draw(
-        parse(code, language),
-        customWidth || 750
-    );
+  return draw(parse(code, language), customWidth || 750);
 }
--- src/themes/default.ts
+++ src/themes/default.ts
@@ -1,24 +1,24 @@
 export enum colors {
-    BackgroundColor = "#151718",
-    DefaultForgroundColor = "#CFD2D1",
-    CloseWindowColor = "#FF5F56",
-    CloseWindowColorStroke = "#E0443E",
-    MinifyWindowColor = "#FFBD2E",
-    MinifyWindowColorStroke = "#DEA123",
-    ReduceWindowColor = "#27C93F",
-    ReduceWindowColorStroke = "#1AAB29",
+  BackgroundColor = "#151718",
+  DefaultForgroundColor = "#CFD2D1",
+  CloseWindowColor = "#FF5F56",
+  CloseWindowColorStroke = "#E0443E",
+  MinifyWindowColor = "#FFBD2E",
+  MinifyWindowColorStroke = "#DEA123",
+  ReduceWindowColor = "#27C93F",
+  ReduceWindowColorStroke = "#1AAB29",
 }
 
 export enum properties {
-    FontSize = 16
+  FontSize = 16,
 }
 
 export const TypeColors = {
-    keyword: "#e6cd69",
-    boolean: "#cd3f45",
-    number: "#cd3f45",
-    string: "#55b5db",
-    'function-variable': "#55b5db",
-    parameter: "#55b5db",
-    function: "#a074c4"
+  keyword: "#e6cd69",
+  boolean: "#cd3f45",
+  number: "#cd3f45",
+  string: "#55b5db",
+  "function-variable": "#55b5db",
+  parameter: "#55b5db",
+  function: "#a074c4",
 } as const;
