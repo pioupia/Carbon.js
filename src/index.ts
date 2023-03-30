367852080353e46e2f15dda7875dc2e4e20508ff
pioupia <pioupiadeux@gmail.com>
2023-03-30 09:40:16 +0200

    Feat (Types): export the languages type

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,12 @@ import type { Grammar } from "prismjs";
 import { parse } from "./util/common";
 import { draw } from "./util/generateImage";
 import CarbonjsError from "./errors/CarbonjsErrors";
-import { languages } from "prismjs";
+import Languages from "./types/Languages";
 
 /**
  * Allows you to generate an Image from code
- * @param {string} code
- * @param {Grammar} language
+ * @param {string} code The code you want to render
+ * @param {Grammar} language The programming language used
  * @param {number=} customWidth The custom with of the image (default: 750px)
  * @return {Canvas}
  */
@@ -19,8 +19,8 @@ export function render(
 ): Canvas {
   if (typeof customWidth === "number" && customWidth <= 100)
     throw new CarbonjsError("The 'customWidth' can't be less than 100.");
-
+  
   return draw(parse(code, language), customWidth || 750);
 }
 
-export const Languages = languages;
+export { Languages };
