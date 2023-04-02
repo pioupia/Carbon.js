e3c2d4d51618e7594dab8c9af3fc45466c66ca71
pioupia <49518790+pioupia@users.noreply.github.com>
2023-04-02 11:16:15 +0200

    Feat (DefaultTheme): Create the default theme with the constructor (#53)

--- src/themes/default.ts
+++ src/themes/default.ts
@@ -1,24 +1,3 @@
-export enum colors {
-  BackgroundColor = "#151718",
-  DefaultForegroundColor = "#CFD2D1",
-  CloseWindowColor = "#FF5F56",
-  CloseWindowColorStroke = "#E0443E",
-  MinifyWindowColor = "#FFBD2E",
-  MinifyWindowColorStroke = "#DEA123",
-  ReduceWindowColor = "#27C93F",
-  ReduceWindowColorStroke = "#1AAB29",
-}
+import { ThemeBuilder } from "../managers/ThemeBuilder";
 
-export enum properties {
-  FontSize = 16
-}
-
-export const TypeColors = {
-  keyword: "#e6cd69",
-  boolean: "#cd3f45",
-  number: "#cd3f45",
-  string: "#55b5db",
-  "function-variable": "#55b5db",
-  parameter: "#55b5db",
-  function: "#a074c4",
-} as const;
+export const defaultTheme = new ThemeBuilder();
