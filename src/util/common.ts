d62a98cafb4ab7fc4caa4c40459b9c1c3392af9b
pioupia <pioupiadeux@gmail.com>
2023-04-02 11:08:39 +0200

    Feat (CommonFunctions): create the deepFreeze and isHexadecimalColor functions

--- src/util/common.ts
+++ src/util/common.ts
@@ -3,3 +3,26 @@ import { tokenize, Grammar } from "prismjs";
 export function parse(code: string, language: Grammar) {
   return tokenize(code.trim(), language);
 }
+
+/**
+ * Verify if a color is a hexadecimal color or not.
+ * @param {string} color
+ * @returns {boolean}
+ */
+export function isHexadecimalColor(color: string): boolean {
+  return /^#([0-9a-f]{3}){1,2}$/i.test(color);
+}
+
+export function deepFreeze<T extends object>(object: T): Readonly<T> {
+  const propNames = Reflect.ownKeys(object);
+
+  for (const name of propNames) {
+    const value = object[name as keyof object];
+
+    if ((value && typeof value === "object") || typeof value === "function") {
+      deepFreeze(value);
+    }
+  }
+
+  return Object.freeze(object);
+}
