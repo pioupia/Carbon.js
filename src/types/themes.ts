f5826240b57be752776fbd2ffb63a2445d6424ab
pioupia <49518790+pioupia@users.noreply.github.com>
2023-04-02 14:40:51 +0200

    Feat (TokenType): add all token types colors and type (#70)

--- src/types/themes.ts
+++ src/types/themes.ts
@@ -27,6 +27,121 @@ interface ThemeDataColorText {
      * Color of a called function (e.g. render(...) -> the 'render' keyword will be colored)
      */
     function: string;
+    /**
+     * Color of builtin functions
+     */
+    builtin: string;
+    /**
+     * Color of the class name
+     */
+    "class-name": string;
+    /**
+     * Color of a char (single character)
+     */
+    char: string;
+    /**
+     *  A primitive data type found in some languages, can be thought of as an identifier.
+     */
+    symbol: string;
+    /**
+     * Color of regular expressions
+     */
+    regex: string;
+    /**
+     * Color of links
+     */
+    url: string;
+    /**
+     * Color of operators (e.g. +, -, -=, > ...)
+     */
+    operator: string;
+    /**
+     * Color of a variable
+     */
+    variable: string;
+    /**
+     * Color of a constant variable
+     */
+    constant: string;
+    /**
+     * Color of an attribute/characteristic or object/map key
+     */
+    property: string;
+    /**
+     * Color of the punctuation such as brackets, parentheses, commas, and more
+     */
+    punctuation: string;
+    /**
+     * Color of important things like:
+     * - !important (in css)
+     * - \# title (in markdown)
+     */
+    important: string;
+    /**
+     * Color of the comments
+     */
+    comment: string;
+    /**
+     * Color of the HTML tags
+     */
+    tag: string;
+    /**
+     * HTML attribute's name
+     */
+    "attr-name": string;
+    /**
+     * HTML attribute's value
+     */
+    "attr-value": string;
+    /**
+     * Color of namespaces
+     */
+    namespace: string;
+    /**
+     * Color of the first part of an XML document
+     */
+    prolog: string;
+    /**
+     * Color of the document type declaration (Markup language)
+     */
+    doctype: string;
+    /**
+     * Color of character data, specific to markup languages
+     */
+    cdata: string;
+    /**
+     * Color of code used to display reserved characters in markup languages
+     * @example
+     * &amp; &#x2665; &#160; &#x152;
+     */
+    entity: string;
+    /**
+     * Color with bold text (Markdown)
+     */
+    bold: string;
+    /**
+     * Color with italic text (Markdown)
+     */
+    italic: string;
+    /**
+     * Color of atrule (`@` rules in stylesheet).
+     * @examples
+     * \@font-family
+     * \@media screen and ...
+     */
+    atrule: string;
+    /**
+     * Color of selector (in stylesheet)
+     */
+    selector: string;
+    /**
+     * Color of inserted text (Mardown)
+     */
+    inserted: string;
+    /**
+     * Color of deleted text (Markdown)
+     */
+    deleted: string;
 }
 
 interface ThemeDataColorWindow {
